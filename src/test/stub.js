c93ec36753033a4f6ca666a383e60c01e01a7c3c
Vincz <vincent@fullstack.pro>
2019-12-21 12:53:10 +0100

    Remove useless test config

--- src/test/stub.js
+++ src/test/stub.js
@@ -16,18 +16,11 @@ const simpleGradient = {
 };
 
 const simpleGradientOneStop = {
-    x1: '17.5001',
-    y1: '32',
-    x2: '17.5001',
-    y2: '2.9711',
     stops: [
         { color: '#FCC3A4', offset: '0', opacity: 0.5 },
     ],
     angle: 0,
-    background: 'rgba(252, 195, 164, 0.5)',
-    string: `<linearGradient xmlns="http://www.w3.org/2000/svg" gradientUnits="userSpaceOnUse" x1="17.5001" y1="32" x2="17.5001" y2="2.9711">
-            <stop offset="0" stop-color="#FCC3A4" stop-opacity="0.5"/>
-        </linearGradient>`,
+    background: 'rgba(252, 195, 164, 0.5)'
 };
 
 export {
